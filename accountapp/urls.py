cd87686801e16884ca19ae25658bba0f30e9e15e
rapael3020 <rapael3020@gmail.com>
2022-04-04 20:54:37 +0900

    Add Delete Page and Implement Withdrawal Request system

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,9 +1,14 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView
+from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
 
 app_name = "accountapp"
+
+
+class AccountUpDeleteView:
+    pass
+
 
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
@@ -12,4 +17,5 @@ urlpatterns = [
     path('create/', AccountCreateView.as_view(), name='create'),
     path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),
     path('update/<int:pk>', AccountUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>', AccountDeleteView.as_view(), name='delete'),
 ]
